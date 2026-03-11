3da3b74bc98b2b51ba62466dfaea1c0810d66686
Istebrek <istebrek.akram@outlook.com>
2026-03-11 20:58:53 +0100

    Create small .NET 10 console application that combines several PDF documents into one.

---
+++ CombinePDFs/Program.cs
@@ -0,0 +1,33 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+string root = Directory.GetCurrentDirectory();
+
+string[] pdfFiles = Directory.GetFiles(root, "*.pdf");
+
+// For specifically ordered files (CTRL + K, CTRL + U to uncomment): 
+/*
+List<string> pdfFiles = new List<string>
+{
+    "FirstFileName.pdf",
+    "SecondFileName.pdf",
+    "ThirdFileName.pdf"
+};
+*/
+
+PdfDocument combinedDocument = new PdfDocument();
+
+if (pdfFiles != null && pdfFiles.Any().Equals(""))
+{
+    foreach (var file in pdfFiles)
+    {
+        PdfDocument currentFile = PdfReader.Open(file, PdfDocumentOpenMode.Import);
+
+        for (int i = 0; i < currentFile.PageCount; i++)
+        {
+            combinedDocument.AddPage(currentFile.Pages[i]);
+        }   
+    }
+
+    combinedDocument.Save("CombinedFileName.pdf");
+}
